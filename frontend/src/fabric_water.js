66e22190132273069f7366e965af1223deafbf18
ctrluserh <harman.saini@mail.utoronto.ca>
2025-01-19 05:16:26 -0500

    Added how much water is used

---
+++ frontend/src/fabric_water.js
@@ -0,0 +1,37 @@
+/**
+ * {
+ * "cotton": 25,
+ * "polyester": 75
+ * }
+ * @param {*} fabrics
+ */
+export function getWater(fabrics) {
+    // Water usage rates (L/g)
+    const waterRates = {
+      "Linen": 2.5,
+      "Cotton": 10,
+      "Polyester": 1 / 100,
+      "Rayon": 10,
+      "Chiffon": 0.5,
+      "Wool": 100,
+      "Jersey": 10,
+      "Leather": 15,
+      "Silk": 5
+    };
+  
+    let water = 0; 
+  
+    // Loop 
+    for (const [fabricName, quantity] of Object.entries(fabrics.fabric)) {
+      const normalizedFabricName = fabricName.charAt(0).toUpperCase() + fabricName.slice(1).toLowerCase();
+      
+      // Check 
+      if (waterRates[normalizedFabricName]) {
+        // Multiply with water rate
+        water += waterRates[normalizedFabricName] * quantity;
+      }
+    }
+  
+    return water;
+  }
+  
